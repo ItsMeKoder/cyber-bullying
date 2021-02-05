a30635eba7f285f135d6508ef5444b7ce53c64f3
kavishdev <46088622+kavishdev@users.noreply.github.com>
2021-02-05 16:44:28 +0530

    Update speech.js

--- static/speech.js
+++ static/speech.js
@@ -1,14 +1,14 @@
-const synth = window.speechSynthesis;
+//const synth = window.speechSynthesis;
 
-const textToSpeech = (string,toSpeak=false) => {
-  if (!toSpeak){
-    return
-  }
-  let voice = new SpeechSynthesisUtterance(string);
-  voice.text = string;
-  voice.lang = "en-US";
-  voice.volume = 1;
-  voice.rate = 1;
-  voice.pitch = 1; // Can be 0, 1, or 2
-  synth.speak(voice);
-}
+//const textToSpeech = (string,toSpeak=false) => {
+//  if (!toSpeak){
+//    return
+//  }
+//  let voice = new SpeechSynthesisUtterance(string);
+//  voice.text = string;
+//  voice.lang = "en-US";
+//  voice.volume = 1;
+//  voice.rate = 2;
+//  voice.pitch = 1; // Can be 0, 1, or 2
+//  synth.speak(voice);
+//}
